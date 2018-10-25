61edc74ea67f2b37d3d9de6d8df3ac60d1e41676
Jhonny Michel <jhonnymichel@outlook.com>
2018-10-24 21:44:36 -0300

    Create batch_rename.py

---
+++ batch_rename.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import csv
+
+def rename_files(path, name_map, ext):
+  with open(name_map, 'rb') as csv_map:
+    map_reader = csv.reader(csv_map)
+    for row in map_reader:
+      old_name, new_name = list(row)
+      old_filename = '%s/%s.%s' % (path, old_name, ext)
+      new_filename = '%s/%s.%s' % (path, new_name, ext)
+      try:
+        os.rename(old_filename, new_filename)
+      except Exception as e:
+        print('Rename for file %s failed. Details: ' % old_filename)
+        print(e)
+
+if __name__ == '__main__':
+  path = sys.argv[1]
+  name_map = sys.argv[2]
+  ext = sys.argv[3]
+  rename_files(path, name_map, ext)
